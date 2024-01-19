a096dcd03615d6d241c92f001a5dd3e42aab3b6f
Rishikesh-pal <rishikeshpal203@gmail.com>
2024-01-19 21:28:56 +0530

    Number programs

---
+++ NumberPrograms/Number18.java
@@ -0,0 +1,22 @@
+package NumberPrograms;
+
+import java.util.Scanner;
+
+public class Number18 {
+    public static void main(String[] args) {
+        int n = 0,arm=0,rem,c;
+        System.out.println("Enter any number : ..");
+        Scanner scanner=new Scanner(System.in);
+        n=scanner.nextInt();
+        c=n;
+        while (n>0){
+            rem=n%10;
+            arm=(rem*rem*rem)+arm;
+            n=n/10;
+        }
+        if(  c==arm )
+            System.out.println("Armstrong number");
+        else
+            System.out.println("Not armstrong");
+    }
+}
